e784408bc8ef2336bfce1be471055d0b52a9773b
makredzic <mak.redzic@gmail.com>
2021-01-17 21:59:53 +0100

    Zapocet zad1, uradjeno prvih 5 podrski za kontejner

---
+++ zad1/MojNizInt.cpp
@@ -0,0 +1,42 @@
+#include "MojNizInt.hpp"
+#include <stdexcept>
+
+int MojNizInt::at(const size_t &i) const {
+  if (i > n_-1) throw std::invalid_argument("Pokusaj pristupa kontejneru van granica");
+  return *(p_+i);
+}
+
+MojNizInt::MojNizInt(const MojNizInt &drugi) : n_{drugi.n_}, p_{new int[n_]} {
+  std::copy(drugi.p_, drugi.p_ + drugi.n_, p_);
+}
+
+MojNizInt::MojNizInt(MojNizInt&& drugi) : n_{drugi.n_}, p_{drugi.p_} {
+  drugi.p_ = nullptr; 
+  //drugi.p_ pokazuje na svoje clanove na heapu kao i novi p_
+  //okidanjem destruktora od rvalue MojNizInt-a koji je proslijedjen ovdje
+  //uzrokovat ce da se izbrise memorija na koju pokazuje drugi.p_
+  //zato se ovdje drugi.p_ seta na nullptr
+  drugi.n_ = 0; //ovo je possibly bespotrebno
+}
+
+MojNizInt& MojNizInt::operator=(const MojNizInt& drugi) {
+  if (this == &drugi) return *this;
+
+  delete [] p_;
+  n_ = drugi.n_; 
+  p_ = new int[n_];
+  std::copy(drugi.p_, drugi.p_ + drugi.n_, p_);
+  return *this;
+}
+
+MojNizInt& MojNizInt::operator=(MojNizInt&& drugi) { 
+  delete [] p_;
+
+  n_ = drugi.n_;
+  p_ = drugi.p_;
+  
+  drugi.p_ = nullptr;
+  drugi.n_ = 0; //mozda potpuno bespotrebno
+
+  return *this;
+}
---
+++ zad1/MojNizInt.hpp
@@ -0,0 +1,24 @@
+#pragma once
+#include <algorithm>
+#include <initializer_list>
+
+class MojNizInt {
+  public:
+
+    MojNizInt() = default;
+    MojNizInt(std::initializer_list<int> a) : n_{a.size()}, p_{new int[n_]} {std::copy(std::begin(a), std::end(a), p_);}
+
+    MojNizInt(const MojNizInt&); //copy konstruktor
+    MojNizInt(MojNizInt&& drugi); //move-copy konstruktor
+
+    MojNizInt& operator=(const MojNizInt&); //copy jednako
+    MojNizInt& operator=(MojNizInt&&); 
+
+    ~MojNizInt() {delete []p_;}; //destruktor
+    size_t size() const {return n_;} 
+    int at(const size_t& i) const;
+
+  private:
+    size_t n_{0};
+    int* p_{nullptr};
+};
